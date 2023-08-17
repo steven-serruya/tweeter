3c3de1f14df6c4b28b821c56b4db1051b6e402fa
= <steven.serruya@gmail.com>
2023-08-17 15:27:49 -0500

    added detailed comments to the js files

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,82 +1,98 @@
+// Wait for the document to be fully loaded
+
 $(document).ready(function() {
+  // Fetches tweets from the server
 
   function loadTweets() {
     $.ajax({
-      type: "GET",
-      url: 'http://localhost:8080/tweets',
-      dataType: 'json',
-      success: function(data) {
-        renderTweets(data);
+      type: "GET", // HTTP GET request
+      url: 'http://localhost:8080/tweets', // URL to fetch data from
+      dataType: 'json', // Data type expected from the server
+      success: function(data) { // Callback function on successful fetch
+        renderTweets(data); // Render the fetched tweets on the page
       },
-      error: function() {
-        alert("Error fetching tweets. Please try again later.");
+      error: function() { // Callback function on fetch error
+        alert("Error fetching tweets. Please try again later."); // Show an error alert
       }
     });
   }
 
-  loadTweets();
+  loadTweets(); // Initial call to load tweets
 
-  // Utility function to escape user-provided text
+  // Utility function to escape user-provided text to prevent XSS attacks
 
   const escape = function(str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
+  // Listen for input changes in the tweet text area
+
   $("#tweet-text").on('input', function() {
     $(".error-message").attr('hidden', true);
   });
+
+  // Toggle (show/hide) the new tweet form when the "Write a new tweet" label or down-arrow is clicked
 
   $(".writetweet, .down-arrow").click(function() {
     $(".new-tweet").slideToggle();
   });
 
+  // Listen for form submission
+
   $(".new-tweet form").on("submit", function(event) {
-    event.preventDefault(); // This ensures the form doesn't submit
+    event.preventDefault(); // Prevent the form from submitting traditionally
 
     const tweetText = $('#tweet-text').val();
+    // Check if tweet text is empty
 
     if (!tweetText || tweetText.trim() === "") {
       showErrorMessage("Tweet cannot be empty!");
       return;
     }
+    // Check if tweet text exceeds the limit
 
     if (tweetText.length > 140) {
       showErrorMessage("Tweet is too long!");
       return;
     }
-    $(".error-message").slideUp();
+    $(".error-message").slideUp(); // Hide the error message, make it slide up
 
 
-    const formData = $(this).serialize();
+    const formData = $(this).serialize(); // Serialize form data for the POST request
+
+    // Send the serialized data to the server
 
     $.ajax({
-      type: "POST",
-      url: 'http://localhost:8080/tweets',
-      data: formData,
-      success: function(response) {
-        loadTweets();
-        $('#tweet-text').val('');
+      type: "POST", // HTTP POST request
+      url: 'http://localhost:8080/tweets', // URL to send data to
+      data: formData, // Data to send to the server
+      success: function(response) { // Callback function on successful POST
+        loadTweets(); // Reload tweets
+        $('#tweet-text').val(''); // Clear the tweet text area
       },
-      error: function() {
-        alert("Error posting tweet. Please try again.");
+      error: function() { // Callback function on POST error
+        alert("Error posting tweet. Please try again."); // Show an error alert
       }
     });
   });
 
+  // Renders multiple tweets to the page
 
   const renderTweets = function(tweets) {
-    $('#tweets-container').empty();
+    $('#tweets-container').empty();  // Empty the container
+
 
     for (let tweet of tweets) {
-      const $tweetElement = createTweetElement(tweet);
-      $('#tweets-container').prepend($tweetElement);
+      const $tweetElement = createTweetElement(tweet); // Create tweet element for each tweet
+      $('#tweets-container').prepend($tweetElement); // Add tweet to the top of the container
     }
   };
 
+  // Create a tweet element using the provided tweet data
 
   const createTweetElement = function(tweet) {
-    const timeAgo = $.timeago(new Date(tweet.created_at)); // Use timeago here
+    const timeAgo = $.timeago(new Date(tweet.created_at)); // Calculate how long ago the tweet was posted
 
     const $tweet = $(`
       <article class="maketweet">
@@ -101,7 +117,7 @@ $(document).ready(function() {
       </article>
     `);
 
-    return $tweet;
+    return $tweet; // Return the tweet element
   };
 
 
@@ -109,6 +125,7 @@ $(document).ready(function() {
 
 
 });
+// Function to show an error message on the page
 
 function showErrorMessage(message) {
   $(".error-message p").text(message);  // Inserting the error message
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,27 @@
+// This function waits for the document (HTML content) to be fully loaded before executing.
 $(document).ready(function() {
+
+  // This listens for the "input" event (i.e., when the user types) on the textarea element within the ".new-tweet" class.
   $(".new-tweet textarea").on("input", function() {
+    // It gets the length (number of characters) of the current value of the textarea.
     const inputLength = $(this).val().length;
+
+    // Calculates the number of characters remaining by subtracting the input length from the total allowed (140 characters).
     const charsRemaining = 140 - inputLength;
+    // Logs the number of characters remaining to the console. This can be useful for debugging purposes.
     console.log(charsRemaining);
+
+    // Selects the element with the ID "counter".
     const counter = $("#counter");
+    // Updates the text content of the counter element to show the number of characters remaining.
     counter.text(charsRemaining);
+
+    // If the number of characters remaining is negative (i.e., the user has typed more than the allowed 140 characters)...
     if (charsRemaining < 0) {
+      // ...then it changes the color of the counter text to red.
       counter.css('color', 'red');
     } else {
+      // If the characters are within the limit, it resets the color of the counter text to its default value.
       counter.css('color', '');
     }
   });
